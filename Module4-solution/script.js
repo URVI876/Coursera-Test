608ed7160b4f568a91cfc3699d226c951129c5ae
URVI876 <22002170120007@ljku.edu.in>
2023-01-23 23:00:07 +0530

    Update script.js

--- Module4-solution/script.js
+++ Module4-solution/script.js
@@ -2,7 +2,7 @@ function hello(){
 	Names=['Ahmedabad','mumbai','jalandar','jetpur','surat','jamnagar','rajkot'];
 	
 	for(var i=0;i<Names.length;i++){
-		var FN=Names[i]//.charAt(0).toLowerCase();
+		var FN=Names[i].charAt(0)//.toLowerCase();
 		if(FN==='j'||FN==='J'){
 			console.log("goodbye "+Names[i]);
 		}
